3ab2b190f785918c5b44315e82685b6277b6443c
mpuertob <marcospuertobarrero1@gmail.com>
2021-08-17 13:21:47 +0200

    microservicio terminado

--- src/main/java/es/marcos/microservices/MicroservicesJavaApplication.java
+++ src/main/java/es/marcos/microservices/MicroservicesJavaApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class MicroservicesJavaApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MicroservicesJavaApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(MicroservicesJavaApplication.class, args);
+  }
 
 }
